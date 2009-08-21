8371086b3b44bf65953fe7744c641db639771fb0
Howard Yeh <hayeah@gmail.com>
2009-08-21 14:22:41 -0700

    Timer::Server should be threadsafe

--- lib/timer/server.rb
+++ lib/timer/server.rb
@@ -1,9 +1,10 @@
 require 'dm-core'
-require 'json'
+require 'monitor'
 class ASS::Timer::Server
   DEFAULT_RETRY = 180 # 3 minutes (pretty arbitrary)
 
   class Once
+    self.extend(MonitorMixin)
     include DataMapper::Resource
     property :id, Serial
     property :to, String
@@ -15,37 +16,40 @@ class ASS::Timer::Server
     property :event_time, Integer
 
     class << self
-      # need to be thread-safe
+      # needs to be thread-safe
       def schedule(new_event_time=nil)
-        if @next_time.nil?
-          # first starting up. do scheduling.
-          if o = self.first(:order => [:event_time.asc])
-            @next_time = o.event_time
+        self.synchronize do
+          if @next_time.nil?
+            # first starting up. do scheduling.
+            if o = self.first(:order => [:event_time.asc])
+              @next_time = o.event_time
+            else
+              return
+            end
+          elsif new_event_time && (new_event_time < @next_time)
+            # if the event_time we are told to schedule is more recent than the
+            # currently set next_time, then we need to reschedule.
+            @next_time = new_event_time
+            EM.cancel_timer(@em_timer)
           else
-            return
+            return # do nothing
           end
-        elsif new_event_time && (new_event_time < @next_time)
-          # if the event_time we are told to schedule is more recent than the
-          # currently set next_time, then we need to reschedule.
-          @next_time = new_event_time
-          EM.cancel_timer(@em_timer)
-        else
-          return # do nothing
-        end
 
-        secs_until = @next_time - Time.now.to_i
-        secs_until =  secs_until < 0 ? 0 : secs_until
-        @em_timer = EM.add_timer(secs_until) {
-          begin
-            Once.fire_events
-            @next_time = nil
-            Once.schedule
-          rescue
-            p $!
-          end
-        }
+          secs_until = @next_time - Time.now.to_i
+          secs_until =  secs_until < 0 ? 0 : secs_until
+          @em_timer = EM.add_timer(secs_until) {
+            begin
+              Once.send :fire_events # private method
+              @next_time = nil
+              Once.schedule
+            rescue
+              p $!
+            end
+          }
+        end
       end
 
+      private
       def fire_events
         # first is to fire all events before Time.now
         self.all(:event_time.lte => Time.now.to_i).each { |t|
