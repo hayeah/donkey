d968f6f15863f86f45a4ff0c1d07d1f74308f6bf
Howard Yeh <hayeah@gmail.com>
2010-02-11 22:51:05 -0800

    mailbox

--- lib/donkey.rb
+++ lib/donkey.rb
@@ -3,85 +3,262 @@ require 'mq'
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'monkey/mq'
+
 class Donkey
   require "donkey/uuid"
-  class Error < RuntimeError
+  class << self
+    def channel
+      @channel ||= Donkey::Channel.open
+      @channel
+    end
   end
 
-  attr_reader :name, :channel
+  attr_reader :id, :name, :channel
   def initialize(name)
+    @id = Donkey::UUID.generate
     @name = name
-    @reactors = []
-    @channel = Donkey::Channel.open
+    @channel = Donkey.channel
   end
 
-  def ping
-    @channel.publish(Donkey::Ping.new(self))
-    @reactors.map(&:on_ping)
+  attr_reader :public, :private
+  def create
+    @public  = Route::Public.declare(self)
+    @private = Route::Private.declare(self)
   end
 
-  # def exchange(opts={})
-#     @exchange ||= self.channel.direct(self.name)
-#     @exchange
-#   end
+  def call(to,data,meta={})
+    public.call(to,data,meta)
+  end
 
-  def react(reactor)
-    @reactors << reactor
+  def cast(to,data,meta={})
+    public.cast(to,data,meta)
+  end
+
+  def pop
+    public.pop
   end
 end
 
+module Donkey::Rabbit
+  extend self
 
-require 'forwardable'
+  def vhosts
+    `rabbitmqctl list_vhosts`.split("\n")[1..-2]
+  end
+
+  def bindings(vhost=nil)
+    query("list_bindings",%w(exchange_name queue_name routing_key arguments),vhost)
+  end
+  
+  def queues(vhost=nil)
+    query("list_queues",%w(name durable auto_delete arguments messages_ready messages_unacknowledged messages_uncommitted messages acks_uncommitted consumers transactions memory),vhost).map { |h|
+      to_integers(h, "transactions","messages","messages_unacknowledged","messages_uncommitted","acks_uncommitted","messages_ready","consumers")
+      to_booleans(h,"auto_delete","durable")
+    }
+  end
+
+  
+
+  def exchanges(vhost=nil)
+    query("list_exchanges",%w(name type durable auto_delete arguments),vhost)
+  end
+
+  def connections
+    query("list_connections",%w(node address port peer_address peer_port state channels user vhost timeout frame_max recv_oct recv_cnt send_oct send_cnt send_pend))
+  end
+
+  def restart
+    `rabbitmqctl stop_app`
+    `rabbitmqctl reset`
+    `rabbitmqctl start_app`
+    true
+  end
+
+  private
+  
+  def to_integers(hash,*names)
+    names.each do |name|
+      hash[name] = Integer(hash[name])
+    end
+    hash
+  end
+
+  def to_booleans(hash,*names)
+    names.each do |name|
+      hash[name] = case hash[name]
+                   when "false"
+                     false
+                   when "true"
+                     true
+                   end
+    end
+    hash
+  end
+  
+  def query(cmd,fields,vhost=nil)
+    vhost = vhost ? "-p #{vhost}" : ""
+    `rabbitmqctl #{vhost} #{cmd} #{fields.join " "}`.split("\n")[1..-2].map { |l|
+      items = l.split("\t")
+      h = {}
+      fields.each_with_index do |key,i|
+        h[key] = items[i]
+      end
+      h
+    }
+  end
+end
+
 class Donkey::Channel
+  require 'forwardable'
   extend Forwardable
 
-  def_delegators :@mq, :direct, :fanout, :exchange
+  def_delegators :@mq, :direct, :fanout, :topic, :queue
   
   def self.open(settings={})
-    self.new(default_settings.merge(settings))
+    self.new(settings)
   end
 
   def self.default_settings
     AMQP.settings
   end
 
+  def self.ensure_eventmachine
+    unless EM.reactor_running?
+      Thread.new { EM.run }
+    end
+  end
+
   attr_reader :settings, :mq
   def initialize(settings={})
+    self.class.ensure_eventmachine
     @settings = self.class.default_settings.merge(settings)
     @mq = MQ.new(AMQP.connect(@settings))
   end
 
-  def publish(message,opts={})
+  def publish(name,message,opts={})
+    @mq.publish(name,message,opts)
+  end
+end
+
+class Donkey::Message
+  require 'bert'
+
+  class Call < self
+  end
+
+  class Cast < self
+  end
+
+  TAG_TO_CLASS = {
+    "call" => Call
+  }
+  CLASS_TO_TAG = TAG_TO_CLASS.inject({}) do |h,(k,v)|
+    h[v] = k
+    h
+  end
+
+  def self.tag_to_class(tag)
+    TAG_TO_CLASS[tag] || raise("no class for tag: #{tag}")
+  end
+
+  def self.class_to_tag(klass)
+    CLASS_TO_TAG[klass] || raise("no tag for class: #{self}")
+  end
+
+  def self.tag
+    @tag ||= class_to_tag(self)
+    @tag
+  end
+
+  def tag
+    self.class.tag
+  end
+
+  # def self.decode
     
-  end
-end
+#   end
 
-class Donkey::Ping
-  def initialize(donkey)
-    
-  end
-end
-
-class Donkey::Pong
-  attr_reader :name, :uuid
-  def initialize(name,uuid)
-    @name = name
-    @uuid = uuid
-  end
-end
-
-class Donkey::Reactor
-  attr_reader :donkey, :uuid
-  def initialize(donkey)
-    @donkey = donkey
-    @uuid = Donkey::UUID.generate
+  attr_reader :to, :meta, :data
+  def initialize(to_name,data,meta={})
+    @to   = to_name
+    @data = data
+    @meta = meta
   end
 
-  def name
-    @donkey.name
+  def tagged_data
+    [tag,@data]
   end
   
-  def on_ping
-    Donkey::Pong.new(name,uuid)
+  def payload
+    BERT.encode(self.tagged_data)
+  end
+end
+
+class Donkey::Route
+  attr_reader :donkey
+  def self.declare(donkey)
+    route = self.new(donkey)
+    route.declare
+    route
+  end
+  
+  def initialize(donkey)
+    @donkey = donkey
+  end
+
+  def channel
+    donkey.channel
+  end
+  
+  def declare
+    raise "abstract"
+  end
+
+  protected
+  
+  def publish
+    raise "abstract"
+  end
+
+  class Public < self
+    attr_reader :exchange, :queue
+    def declare
+      @exchange = channel.direct(donkey.name)
+      @queue = channel.queue(donkey.name).bind(donkey.name,:key => "")
+    end
+
+    # gets one message delivered
+    def pop(opts={})
+      @queue.pop(opts) do |header,payload|
+        
+      end
+    end
+
+    def call(to,data,meta={})
+      publish(Donkey::Message::Call.new(to,data,meta))
+    end
+
+    def cast(to,data,meta={})
+      publish(Donkey::Message::Cast.new(to,data,meta))
+    end
+
+    private
+    
+    def publish(message)
+      channel.publish(message.to,message.payload,message.meta.merge(:key => ""))
+    end
+  end
+
+  class Private < self
+    def declare
+      @id = donkey.id
+      @exchange = channel.direct(donkey.name)
+      @queue = channel.queue(@id,:auto_delete => true).bind(donkey.name,:key => @id)
+    end
+
+    def publish(message)
+      channel.publish(message.to,message.payload,:key => message.id)
+    end
   end
 end
--- spec/donkey_spec.rb
+++ spec/donkey_spec.rb
@@ -2,121 +2,190 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Donkey" do
   before(:each) do
-    stub(AMQP).connect.with_any_args
-    stub(MQ).new.with_any_args
-    @channel = Donkey::Channel.open
-    stub(Donkey::Channel).open { @channel }
+    @channel = Object.new
+    stub(Donkey).channel { @channel }
+    stub(Donkey::UUID).generate { @uuid = "uuid" }
+    @donkey = Donkey.new("name")
+    @public = Object.new
+    @private = Object.new
+    stub(@donkey).public { @public }
+    stub(@donkey).private { @private }
+  end
+  
+  it "creates routes" do
+    mock(Donkey::Route::Public).declare(@donkey)
+    mock(Donkey::Route::Private).declare(@donkey)
+    @donkey.create
+  end
 
-    @name = "testing server"
-    @donkey = Donkey.new(@name)
-    @reactor = Donkey::Reactor.new(@name)
+  it "has id" do
+    @donkey.id.should == @uuid
   end
 
   it "has name" do
-    @donkey.name.should == @name
+    @donkey.name.should == "name"
+  end
+
+  it "uses default channel" do
+    @donkey.channel.should == Donkey.channel
+  end
+
+  it "calls" do
+    mock(@public).call(*args = ["to","data",{"foo" => "bar"}])
+    @donkey.call(*args)
+  end
+
+  it "casts" do
+    mock(@public).cast(*args = ["to","data",{"foo" => "bar"}])
+    @donkey.cast(*args)
+  end
+
+  it "pops a message" do
+    pending
+    mock(@donkey.private).pop
+    @donkey.pop
+  end
+end
+
+describe "Donkey::Route" do
+  before(:each) do
+    @channel = Object.new
+    stub(Donkey).channel { @channel }
+    @donkey = Donkey.new("name")
+  end
+
+  context "Public" do
+    before(:each) do
+      @exchange = Object.new
+      @queue = Object.new
+      mock(@channel).direct(@donkey.name) { @exchange }
+      mock(@channel).queue(@donkey.name) { mock!.bind(@donkey.name,:key => ""){ @queue }.subject }
+      @public = Donkey::Route::Public.declare(@donkey)
+    end
+    
+    it "declares" do
+      @public.queue.should == @queue
+      @public.exchange.should == @exchange
+    end
+
+    it "publishes" do
+      msg = Object.new
+      mock(msg).to { "to" }
+      mock(msg).payload { "payload" }
+      meta = { :foo => :bar }
+      mock(msg).meta { meta }
+      mock(@channel).publish("to","payload",meta.merge(:key => ""))
+      @public.send(:publish,msg)
+    end
+
+    it "calls" do
+      mock(@public).publish(is_a(Donkey::Message::Call))
+      @public.call("to","data",:foo => :bar)
+    end
+
+    it "casts" do
+      mock(@public).publish(is_a(Donkey::Message::Cast))
+      @public.cast("to","data",:foo => :bar)
+    end
+  end
+
+  context "Private" do
+    it "delcares" do
+      # FIX I don't know how not to test the implementation
+      mock(@channel).direct(@donkey.name)
+      mock(@channel).queue(@donkey.id,:auto_delete => true) { mock!.bind(@donkey.name,:key => @donkey.id).subject }
+      r = Donkey::Route::Private.declare(@donkey)
+    end
+
+    it "publishes" do
+      
+    end
+  end
+end
+
+describe "Donkey::Message" do
+  it "raises if a class has no associated tag" do
+    new_message_class = Class.new(Donkey::Message)
+    lambda { new_message_class.tag }.should raise_error
+  end
+
+  it "raises if a tag has no associated class" do
+    lambda { Donkey::Message.tag_to_class("fwajelkfjlfla") }.should raise_error
   end
   
-  # it "declares exchange" do
-#     mock(@channel).direct(@donkey.name)
-#     @donkey.exchange
-#     @donkey.
-#   end
+  context "Call" do
+    before(:each) do
+      @to="name"
+      @data = "data"
+      @call = Donkey::Message::Call.new(@to,@data)
+    end
 
-#   it "sets exchange once only" do
-#     mock(@channel).direct(@donkey.name).once
-#     @donkey.exchange
-#     @donkey.exchange
-#   end
-  
-  context "react" do
+    it "has tag" do
+      @call.tag.should == "call"
+    end
+
+    it "tags data to indicate type" do
+      @call.tagged_data.should be_an(Array)
+      @call.tagged_data[0].should == @call.tag
+      @call.tagged_data[1].should == @data
+    end
+
+    it "encodes data" do
+      mock(@call).tagged_data { "tagged-data" }
+      mock(BERT).encode("tagged-data") { "bert" }
+      @call.payload.should == "bert"
+    end
+
+    # it "replies to donkey" do
+#       pending
+#       reply = @call.reply("reply")
+#       reply.should 
+#     end
     
   end
-  
-  context "ping" do
-    it "has no instance reacting" do
-      @donkey.ping.should == []
-    end
 
-    it "sends ping into rabbitmq" do
-      mock(@channel).publish(is_a(Donkey::Ping)).once
-      @donkey.ping
-    end
-
-    it "has one instance reacting" do
-      mock(@reactor).on_ping.once
-      @donkey.react @reactor
-      @donkey.ping.should have(1).pongs
-    end
-
-    it "has two instances reacting" do
-      mock(@reactor).on_ping.twice
-      @donkey.react @reactor
-      @donkey.react @reactor
-      @donkey.ping.should have(2).pongs
-    end
-  end
+#   context "Cast" do
+#     before(:each) do
+#       @donkey = Object.new
+#       @to="destination"
+#       @data = "data"
+#       @cast = Donkey::Message::Cast.new(@donkey,@to,@data)
+#     end
+#   end
 end
 
 describe "Donkey::Channel" do
-  before(:each) do
-    @_amqp = mock(AMQP).connect(is_a(Hash))
-    @_mq = mock(MQ).new.with_any_args
+  context "open" do
+    it "uses default settings to initialize AMQP" do
+      mock(AMQP).connect(Donkey::Channel.default_settings)
+      mock(MQ).new.with_any_args
+      mock(Donkey::Channel).ensure_eventmachine
+      Donkey::Channel.open
+    end
+
+    it "overrides default settings to initialize AMQP" do
+      mock(AMQP).connect(Donkey::Channel.default_settings.merge({ :overrided_setting => "overrided_value"}))
+      mock(MQ).new.with_any_args
+      Donkey::Channel.open({:overrided_setting => "overrided_value"})
+    end
+    
   end
 
-  it "creates a new AMQP channel" do
-    Donkey::Channel.open
-  end
+  # FIXME dunno how to test this
+  # context "ensure eventmachine" do
+#     it "starts if not running" do
+#       stub(EM).reactor_running? { false }
+#       mock(EM).run
+#       mock(Thread).new
+#       Donkey::Channel.ensure_eventmachine
+#     end
 
-  it "uses default settings to initialize AMQP" do
-    @_amqp.with(Donkey::Channel.default_settings)
-    Donkey::Channel.open
-  end
-
-  it "overrides default settings to initialize AMQP" do
-    @_amqp.with(Donkey::Channel.default_settings.merge({ :overrided_setting => "overrided_value"}))
-    Donkey::Channel.open({:overrided_setting => "overrided_value"})
-  end
-end
-
-describe "Donkey::Reactor" do
-  before(:each) do
-    stub(AMQP).connect
-    stub(MQ).new
-    @name = "testing reactor"
-    @donkey = Donkey.new(@name)
-    @reactor = Donkey::Reactor.new(@donkey)
-  end
-
-  it "belongs to donkey" do
-    @reactor.donkey == @donkey
-  end
-
-  it "has same name its donkey" do
-    @reactor.name.should == @reactor.donkey.name
-  end
-
-  it "has uuid" do
-    guids = ["uuid 1","uuid 2"]
-    mock(Donkey::UUID).generate.twice { guids.shift }
-    Donkey::Reactor.new(@donkey).uuid.should == "uuid 1"
-    Donkey::Reactor.new(@donkey).uuid.should == "uuid 2"
-  end
+#     it "does nothing if already running" do
+#       stub(EM).reactor_running? { true }
+#       dont_allow(EM).run
+#       dont_allow(Thread).new
+#       Donkey::Channel.ensure_eventmachine
+#     end
+#   end
   
-  context "on_ping" do
-    before(:each) do
-      @pong = @reactor.on_ping
-    end
-
-    it "returns a pong" do
-      @pong.should be_a(Donkey::Pong)
-    end
-
-    it "passes name to pong" do
-      @pong.name.should == @reactor.name
-    end
-
-    it "passes uuid to pong" do
-      @pong.uuid.should == @reactor.uuid
-    end
-  end
 end
