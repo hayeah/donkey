b3c3fb84df7229ae4aa86bc4ae6299e797f9d3bc
Howard Yeh <hayeah@gmail.com>
2010-02-26 18:21:47 -0800

    waiter

--- lib/donkey.rb
+++ lib/donkey.rb
@@ -133,47 +133,103 @@ class Donkey::Ticket < Struct.new(:value)
 end
 
 class Donkey::Waiter
+  class Error < Donkey::Error
+  end
+  class NotReceived < Error
+  end
+
+  class AlreadySignaled < Error
+  end
+
+  class TimeoutAlreadySet < Error
+  end
+  
   attr_reader :pending, :received
-  def initialize(*keys,&block)
-    @sucess_callback = block
+  attr_reader :success_callback
+  def initialize(waiter_map,*keys,&block)
+    @success_callback = block
     @keys = keys
     @pending  = Set.new(@keys)
     @received = {} # map(key => value)
+    @waiter_map = waiter_map
+    @waiter_map.register(self,*keys)
   end
 
-  def timeout
-    
+  attr_reader :timeout_callback, :timer
+  def timeout(time,&block)
+    raise TimeoutAlreadySet,self if @timer
+    @timer = EM.add_timer(time) {on_timeout}
+    @timeout_callback = block
   end
 
   def ready?
     @pending.empty?
   end
-  
-  private
-  def on_timeout
-    @tickets.each do |ticket|
-      self.class.unregister(ticket)
-    end
-    @done = true
-    if not done? && @timeout_callback
-      @timeout_callback.call(self)
+
+  def received?(key)
+    @received.has_key?(key)
+  end
+
+  def value(key)
+    if received?(key)
+      @received[key]
+    else
+      raise NotReceived, key
     end
   end
 
-  def signal(ticket,value)
-    @tickets.delete(ticket)
-    if not done? && @tickets.empty?
-      @done = true
-      on_success
+  def values
+    @keys.map { |key| value(key) }
+  end
+
+  def signal(key,value)
+    # double signaling should not happen..?
+    raise AlreadySignaled, key if received?(key)
+    @pending.delete(key)
+    @received[key] = value
+    on_success if @pending.empty?
+  end
+
+  def status
+    @status
+  end
+  
+  def done?
+    not @status.nil?
+  end
+
+  def success?
+    status == :success
+  end
+
+  def timeout?
+    status == :timeout
+  end
+
+  private
+
+  def complete(status,&block)
+    return if done?
+    @status = status
+    block.call
+    @waiter_map.unregister(self,*@keys)
+    timer.cancel if timer
+  end
+  
+  def status=(status)
+    @status = status
+  end
+  
+  def on_timeout
+    complete(:timeout) do
+      timeout_callback.call(self)
     end
   end
 
   def on_success
-    @success_callback.call(values)
-  end
-
-  def done?
-    @done == true
+    complete(:success) do
+      success_callback.call(*values)
+    end
   end
 end
 
@@ -351,8 +407,6 @@ class Donkey::Message
   
   class Cast < self
   end
-
-  
 
   TAG_TO_CLASS = {
     "call" => Call,
